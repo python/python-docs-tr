49e0c504f88e811f5358701bf8e44282487e68d1
egeakman <me@egeakman.dev>
2023-02-11 02:10:06 +0300

    try pre-commit on multiple platforms

--- .pre-commit-scripts/install_gettext.py
+++ .pre-commit-scripts/install_gettext.py
@@ -21,10 +21,10 @@ https://github.com/python/python-docs-tr/blob/HEAD/wiki/gettext.md for instructi
             )
             exit(0)
     elif sys.platform in ["linux", "linux2"]:
-        cmd = shlex.split("sudo apt --upgrade && sudo apt install -y gettext", posix=True)
+        cmd = shlex.split("sudo apt --update && sudo apt install -y gettext", posix=True)
     else:  # macOS
         cmd = shlex.split("brew update && brew install gettext", posix=True)
-    return subprocess.run(cmd, shell=True, check=True)
+    return subprocess.run(cmd, shell=False, check=True)
 
 
 if __name__ == "__main__":
