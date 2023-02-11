cd3c3985f5fbcdfce53066c680d96b1b401c38d3
egeakman <me@egeakman.dev>
2023-02-11 03:20:40 +0300

    let's try this

--- .pre-commit-scripts/install_gettext.py
+++ .pre-commit-scripts/install_gettext.py
@@ -7,25 +7,19 @@ import subprocess
 
 def main():
     if sys.platform == "win32":
-        try:
-            cmd = shlex.split(
-                "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser;irm get.scoop.sh | iex;scoop install gettext", posix=False
-            )
-            return subprocess.run(cmd, shell=True, check=True, executable=shutil.which("powershell"))
-        except Exception:
-            print(
-                NotImplementedError(
-                    "WARNING: Use scoop to install gettext on Windows!!! Otherwise powrap will fail.\nRefer to \
-https://github.com/python/python-docs-tr/blob/HEAD/wiki/gettext.md for instructions."
-                )
-            )
-            exit(0)
+        executable = shutil.which("powershell.exe")
+        cmd = shlex.split("Set-ExecutionPolicy RemoteSigned -Scope CurrentUser;irm get.scoop.sh | iex", posix=False)
+        p_scoop = subprocess.run(cmd, shell=True, executable=executable)
+        p_install = subprocess.run(shlex.split("scoop install gettext", posix=False), shell=True, executable=executable)
+
+        return p_scoop.returncode or p_install.returncode
+
     elif sys.platform in ["linux", "linux2"]:
         cmd = "sudo apt update && sudo apt install -y gettext"
     else:  # macOS
         cmd = "brew update && brew install gettext"
-    return subprocess.run(cmd, shell=True, check=True)
+    return subprocess.run(cmd, shell=True).returncode
 
 
 if __name__ == "__main__":
-    exit(main().returncode)
+    exit(main())
