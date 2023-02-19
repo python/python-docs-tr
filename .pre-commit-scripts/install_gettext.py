50dde37edc4d5ad17fbe7f229ae1511f8cff202c
egeakman <me@egeakman.dev>
2023-02-19 18:11:12 +0300

    Let's see how this'll do

--- .pre-commit-scripts/install_gettext.py
+++ .pre-commit-scripts/install_gettext.py
@@ -1,18 +1,39 @@
 #!/usr/bin/env python3
+import os
 import sys
 import shlex
 import shutil
+import requests
 import subprocess
+from zipfile import ZipFile
 
 
 def main():
     if sys.platform == "win32":
-        executable = shutil.which("powershell.exe")
-        cmd = shlex.split("Set-ExecutionPolicy RemoteSigned -Scope CurrentUser;irm get.scoop.sh | iex", posix=False)
-        p_scoop = subprocess.run(cmd, shell=True, executable=executable)
-        p_install = subprocess.run(shlex.split("scoop install gettext", posix=False), shell=True, executable=executable)
+        powershell = shutil.which("powershell.exe")
+        gettext_url = "https://github.com/vslavik/gettext-tools-windows/releases/download/v0.21.1/gettext-tools-windows-0.21.1.zip"
+        gettext_path = os.path.join(os.environ["LOCALAPPDATA"], "Programs", "gettext-tools")
+        gettext_zip = os.path.join(gettext_path, "gettext-tools.zip")
+        
+        try:
+            subprocess.run(shlex.split(f"mkdir -p {gettext_path}", posix=False), shell=True, executable=powershell)
+            
+            response = requests.get(gettext_url)
+            with open(gettext_zip, "wb") as f:
+                f.write(response.content)
+            
+            with ZipFile(gettext_zip, "r") as zip:
+                zip.extractall(gettext_path)
+                
+            os.remove(gettext_zip)
+            os.environ["PATH"] += os.pathsep + os.path.join(gettext_path, "bin")
 
-        return p_scoop.returncode or p_install.returncode
+        except Exception as e:
+            print(e)
+            return 1
+        
+        else:
+            return 0
 
     elif sys.platform in ["linux", "linux2"]:
         cmd = "sudo apt update; sudo apt install -y gettext"
@@ -22,5 +43,5 @@ def main():
 
 
 if __name__ == "__main__":
-    print(".")
+    print("Installing gettext...")
     exit(main())
